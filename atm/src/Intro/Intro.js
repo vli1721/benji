69f186e9e9ba611dd6395195375382cce07dafd8
Brian Yu <brian@byu.io>
2019-01-10 15:48:14 -0500

    face stuff

--- atm/src/Intro/Intro.js
+++ atm/src/Intro/Intro.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Intro.css';
 
+import {brian_1, brian_2} from '../Descriptors/Brian.js';
+import {vincent_1, vincent_2} from '../Descriptors/Vincent.js';
+
 import * as faceapi from 'face-api.js';
 import '@tensorflow/tfjs';
 
@@ -14,6 +17,7 @@ class Intro extends Component {
 
     this.state = {
       numFaces: null,
+      face: null,
     }
 
     this.mtcnnForwardParams = {
@@ -36,57 +40,38 @@ class Intro extends Component {
     this.onPlay = this.onPlay.bind(this);
   }
 
-  // async loadModels () {
-  //   //await faceapi.loadModels(MODEL_URL)
-  //   await faceapi.loadFaceDetectionModel(MODEL_URL)
-  //   await faceapi.loadFaceLandmarkModel(MODEL_URL)
-  //   await faceapi.loadFaceRecognitionModel(MODEL_URL)
-  // }
-
-  // getFullFaceDescription = async (canvas) => {
-  //   console.log(canvas);
-  //   this.fullFaceDescriptions = await faceapi.allFaces(canvas, minConfidence);
-  //   console.log(this.fullFaceDescriptions);
-  // }
-
-  // drawDescription = (canvas) => {
-  //   this.fullFaceDescriptions.forEach((fd, i) => {
-  //     faceapi.drawLandmarks(canvas, fd.landmarks, { drawLines: false })
-  //   })
-  // }
-
-  // drawHTMLImage(canvas,image,width,height){
-  //   const ctx = canvas.getContext("2d");
-  //   ctx.drawImage(image,0,0,width,height);
-  // }
-
-  // async componentDidMount() {
-  //   await this.loadModels();
-  //   // const testImageHTML = document.getElementById('test')
-  //   // this.drawHTMLImage(this.refs.canvas.current,testImageHTML,296,296);
-  //   await this.getFullFaceDescription(this.refs.canvas.current);
-  //   this.drawDescription(this.refs.canvas.current);
-  // }
-
-  // landmarkWebCamPicture = (picture) => {
-  //   const ctx = this.refs.canvas.current.getContext("2d");
-  //   var image = new Image();
-  //   image.onload = async () => {
-  //     ctx.drawImage(image,0,0);
-  //     await this.getFullFaceDescription(this.refs.canvas.current);
-  //     this.drawDescription(this.refs.canvas.current);
-  //   };
-  //   image.src = picture;
-  // }
-
   async loadModels () {
-    // await faceapi.loadModels(MODEL_URL)
     await faceapi.loadFaceDetectionModel(MODEL_URL);
+    await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
     await faceapi.loadFaceRecognitionModel(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceLandmarkTinyModel(MODEL_URL)
   }
 
   async componentDidMount() {
     await this.loadModels();
+
+    const brianDescriptors = [brian_1, brian_2]
+    const vincentDescriptors = [vincent_1, vincent_2]
+
+
+    const labeledDescriptors = [
+      new faceapi.LabeledFaceDescriptors(
+        'brian',
+        brianDescriptors.map(x => new Float32Array(Object.values(x)))
+      ),
+      new faceapi.LabeledFaceDescriptors(
+        'vincent',
+        vincentDescriptors.map(x => new Float32Array(Object.values(x)))
+      ),
+    ]
+
+    // console.log(Object.keys(brian_1))
+    // console.log(new Float32Array(Object.values(brian_1)))
+
+    this.faceMatcher = new faceapi.FaceMatcher(labeledDescriptors)
+
+
 
     console.log(faceapi.nets)
     
@@ -104,23 +89,6 @@ class Intro extends Component {
     this.onPlay();
   }
 
-  resizeCanvasAndResults(dimensions, canvas, results) {
-    const { width, height } = dimensions instanceof HTMLVideoElement
-      ? faceapi.getMediaDimensions(dimensions)
-      : dimensions
-    canvas.width = width
-    canvas.height = height
-
-    // resize detections (and landmarks) in case displayed image is smaller than
-    // original size
-    return faceapi.resizeResults(results, { width, height })
-  }
-
-  drawDetections(dimensions, canvas, detections) {
-    const resizedDetections = this.resizeCanvasAndResults(dimensions, canvas, detections)
-    faceapi.drawDetection(canvas, resizedDetections)
-  }
-
   async onPlay() {
 
     const input = this.refs.video;
@@ -131,31 +99,33 @@ class Intro extends Component {
     const canvas = this.refs.canvas;
 
     const detections = await faceapi.detectAllFaces(input)
+    .withFaceLandmarks(true)
+    .withFaceDescriptors()
 
     if (detections.length < 1) {
       return setTimeout(() => this.onPlay())
-    }
+    } 
 
-    // resize the detected boxes in case your displayed image has a different size then the original
-    // const detectionsForSize = faceapi.resizeResults(detections, { width: input.width, height: input.height })
-    // draw them into a canvas
-    canvas.width = input.width
-    canvas.height = input.height
-
-    const ctx = canvas.getContext("2d");
     console.log(detections)
-    // faceapi.drawDetection(canvas, detections, { withScore: true })
 
+    const bestMatch = this.faceMatcher.findBestMatch(detections[0].descriptor)
+
+    console.log(bestMatch.toString())
+
+    // faceapi.drawDetection(canvas, detections, { withScore: true })
     // ctx.rect(20, 20, 150, 100);
     // ctx.stroke();
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'red';
-    const box = detections[0].box
+    // ctx.lineWidth = 3;
+    // ctx.strokeStyle = 'red';
+    // const box = detections[0].box
+    // ctx.strokeRect(box.x, box.y, box.width, box.height);
+
     this.setState({
-      numFaces: detections.length
+      numFaces: detections.length,
+      face: bestMatch,
     })
-    ctx.strokeRect(box.x, box.y, box.width, box.height);
-    setTimeout(() => this.onPlay())
+
+    // setTimeout(() => this.onPlay())
 
   }
 
@@ -168,7 +138,12 @@ class Intro extends Component {
           <video id="inputVideo" ref="video" autoPlay={true} muted></video>
           <canvas id="overlay" ref="canvas" />
         </div>
-        { this.state.numFaces != null && <h2>{this.state.numFaces} faces detected</h2>}
+        <h2>
+          { this.state.numFaces == null ? "Detecting..." : `${this.state.numFaces} faces detected` }
+        </h2>
+        <h2>
+          { this.state.numFaces != null && `Best match: ${this.state.face}` }
+        </h2>
       </div>
     );
   }
