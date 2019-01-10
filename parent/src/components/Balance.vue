c102a62b4f8abc0918445173d2855e5f9387deb2
Albert Zhang <albertz3@uci.edu>
2019-01-10 11:42:48 -0500

    fix

---
+++ parent/src/components/Balance.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-sheet
+                class="d-flex"
+                color="green lighten-3"
+                height="424"
+        >
+
+            <p style="margin: 20px; font-size: 24px">Balance: {{'$'+balance}}</p>
+        </v-sheet>
+
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                balance: 20
+            }
+        }
+    }
+</script>
