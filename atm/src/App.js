4087ab98ecbadc80228a238e522d0e80a4b62f3c
Brian Yu <brian@byu.io>
2019-01-10 11:07:06 -0500

    Connect to firebase

--- atm/src/App.js
+++ atm/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Intro from './Intro/Intro.js';
+import Balance from './Balance/Balance.js';
 import firebase from "firebase";
 
 class App extends Component {
@@ -11,7 +12,10 @@ class App extends Component {
     this.state = {
       page: Intro,
     }
-    var config = {
+  }
+
+  initFirebase = () => {
+    this.config = {
       apiKey: "AIzaSyDa8Xluwh_e0fp-vVjyoZxDqekd7IcAoCk",
       authDomain: "benji-42f8d.firebaseapp.com",
       databaseURL: "https://benji-42f8d.firebaseio.com",
@@ -19,7 +23,27 @@ class App extends Component {
       storageBucket: "benji-42f8d.appspot.com",
       messagingSenderId: "533301633340"
     };
-    firebase.initializeApp(config);
+    firebase.initializeApp(this.config);
+    this.database = firebase.database();
+    this.pageRef = firebase.database().ref('atm_page');
+    this.pageRef.on('value', (snapshot) => {
+      let page = Intro;
+      switch (snapshot.val()) {
+        case 'intro':
+          page = Intro;
+          break
+        case 'balance':
+          page = Balance
+          break
+        default:
+          page = Intro;
+      }
+      this.setState({page: page});
+    });
+  }
+
+  componentDidMount = () => {
+    this.initFirebase();
   }
 
   render() {
---
+++ atm/src/Balance/Balance.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Balance.css';
+
+class Balance extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="balance">
+        <h1>Balance page</h1>
+      </div>
+    );
+  }
+}
+
+export default Balance;
