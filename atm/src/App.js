635801138afd92eb877dced6d647ebafe1fa1b94
Brian Yu <brian@byu.io>
2019-01-10 10:55:59 -0500

    Add firebase

--- atm/src/App.js
+++ atm/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Intro from './Intro/Intro.js';
+import firebase from "firebase";
 
 class App extends Component {
 
@@ -10,6 +11,15 @@ class App extends Component {
     this.state = {
       page: Intro,
     }
+    var config = {
+      apiKey: "AIzaSyDa8Xluwh_e0fp-vVjyoZxDqekd7IcAoCk",
+      authDomain: "benji-42f8d.firebaseapp.com",
+      databaseURL: "https://benji-42f8d.firebaseio.com",
+      projectId: "benji-42f8d",
+      storageBucket: "benji-42f8d.appspot.com",
+      messagingSenderId: "533301633340"
+    };
+    firebase.initializeApp(config);
   }
 
   render() {
