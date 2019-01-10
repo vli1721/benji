47bcc8f3a491185681c8e1586d2027cf05af91e9
Albert Zhang <albertz3@uci.edu>
2019-01-10 11:24:26 -0500

    base

--- parent/src/App.vue
+++ parent/src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-container>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+      <v-sheet
+              class="d-flex"
+              color="green lighten-3"
+              height="424"
+      >
+
+        <p>Balance</p>
+      </v-sheet>
+
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data () {
     return {
       //
