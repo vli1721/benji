a5583b143660b60b0df44e41888dd943923525f4
Vincent Li <vincentli2014@gmail.com>
2019-01-10 19:46:43 -0500

    added .env file

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 from flask_cors import CORS
 
+import pyrebase  # Python firebase API
+
+import dialogflow_v2 as dialogflow  # Dialogflow Python SDK
 
 # Configure application
 app = Flask(__name__)
 CORS(app) # enable cross-origin requests
 
-import pyrebase
 
 config = {
   "apiKey": "AIzaSyDa8Xluwh_e0fp-vVjyoZxDqekd7IcAoCk",
@@ -15,6 +17,11 @@ config = {
   "databaseURL": "https://benji-42f8d.firebaseio.com",
   "storageBucket": "benji-42f8d.appspot.com",
 }
+
+
+session_client = dialogflow.SessionsClient()
+DIALOGFLOW_PROJECT_ID = "benji-42f8d"
+DIALOGFLOW_LANGUAGE_CODE = "en"
 
 firebase = pyrebase.initialize_app(config)
 
@@ -31,10 +38,40 @@ chores = db.child("chores")
 def index():
 	return render_template("stt_test.html")
 
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+
+
 @app.route("/post_stt", methods=["POST"])
 def post_stt():
 	request_json = request.get_json()
 	print(request_json)
+	texts = "My name is " + str(request_json.name) + ". " + request_json.query
+	detect_intent_texts(DIALOGFLOW_PROJECT_ID, 1, texts, DIALOGFLOW_LANGUAGE_CODE)
 	return "Request received post_stt"
 
 @app.route("/update_chore", methods=["POST"])
