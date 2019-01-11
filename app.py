6e50daffd07616b3b10ea5f693b46ed066b6b148
Vincent Li <vincentli2014@gmail.com>
2019-01-10 20:09:46 -0500

    updated detect intent

--- app.py
+++ app.py
@@ -50,22 +50,21 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
 
-    for text in texts:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
+    text_input = dialogflow.types.TextInput(
+        text=texts, language_code=language_code)
 
-        query_input = dialogflow.types.QueryInput(text=text_input)
+    query_input = dialogflow.types.QueryInput(text=text_input)
 
-        response = session_client.detect_intent(
-            session=session, query_input=query_input)
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
 
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text))
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
 
 
 @app.route("/post_stt", methods=["POST"])
